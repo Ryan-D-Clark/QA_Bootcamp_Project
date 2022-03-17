aea2346bfc72e2a1f0990a9be956cbdf624da337
Ryan-D-Clark <Ryan@clarkonline.me.uk>
2022-03-17 22:31:37 +0000

    QAP-53 QAP-50 QAP-34 QAP-36 QAP-47 #done RC: Finished ReptileControllerUnitTest.java

---
+++ reptileApplication/src/main/java/main/reptileApplication/exception/ReptileNotFoundException.java
@@ -0,0 +1,11 @@
+//package main.reptileApplication.exception;
+//
+//import org.apache.wink.common.http.HttpStatus;
+//import org.springframework.web.bind.annotation.ResponseStatus;
+//
+//import javax.persistence.EntityNotFoundException;
+//
+//@ResponseStatus
+//public class ReptileNotFoundException extends EntityNotFoundException {
+//    @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Band does not exist with that ID");
+//}
